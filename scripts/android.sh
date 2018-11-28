ef3322bc75e7a06541183a44310bd5db875cff2b
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-11-28 14:15:36 +0100

    Update android.sh

--- scripts/android.sh
+++ scripts/android.sh
@@ -2,7 +2,7 @@
 sudo docker pull quay.io/quamotion/android-x86-disk:7.1-r2
 sudo docker pull quay.io/quamotion/android-x86-disk:8.1-rc2
 sudo docker pull quay.io/quamotion/android-x86-hook:master
-sudo docker pull quay.io/quamotion/android-x86-launcher:master
+sudo docker pull quay.io/quamotion/android-x86-launcher:0.1-qemu-3.1.0-rc1
 
 # This will force kubevirt to use the Android launcher. Use with care
-sudo docker tag quay.io/quamotion/android-x86-launcher:master docker.io/kubevirt/virt-launcher:v0.10.0
+sudo docker tag quay.io/quamotion/android-x86-launcher:0.1-qemu-3.1.0-rc1 docker.io/kubevirt/virt-launcher:v0.10.0
