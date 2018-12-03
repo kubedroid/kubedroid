ead6a8802fe808a40035cb2b9ac582a0f9a78f0b
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-12-03 17:59:26 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,15 +8,6 @@ add-apt-repository "deb https://download.docker.com/linux/$(. /etc/os-release; e
 apt-get update
 apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 18.06 | head -1 | awk '{print $3}')
 
-# Install kubectl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-
-apt-get update
-apt-get install -y kubectl
-
 # Configure minikube
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl
