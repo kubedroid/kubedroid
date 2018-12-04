1b08535c6ba5e6dd34b0eb55393e22b73746197c
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-12-04 17:42:18 +0100

    Update kubevirt.sh

--- scripts/kubevirt.sh
+++ scripts/kubevirt.sh
@@ -2,8 +2,8 @@
 RELEASE=v0.10.0
 kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/${RELEASE}/kubevirt.yaml
 
-curl -L https://github.com/kubevirt/kubevirt/releases/download/v0.9.6/virtctl-v0.9.6-linux-amd64 -o /usr/local/bin/virtctl
+curl -L https://github.com/kubevirt/kubevirt/releases/download/${RELEASE}/virtctl-${RELEASE}-linux-amd64 -o /usr/local/bin/virtctl
 chmod +x /usr/local/bin/virtctl
 
-kubectl create -f gpu_plugin.yaml
+kubectl create -f vgpu_plugin.yaml
 
